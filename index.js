23ba8483f278ac8bbcd43b89f5c20191c5095368
kipa00 <k2pa00@gmail.com>
2020-05-21 05:01:50 +0900

    판을 이동할 수 있도록 수정

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 var board_num = [];
 var can_place = true;
 var info = undefined;
+var pos_x = undefined, pos_y = undefined;
+var click_x = undefined, click_y = undefined, clicked = false;
 
 function init() {
   var board = document.getElementById('board');
-  for (var i=0; i<9; ++i) {
-    var tr_elem = document.createElement('tr');
-    for (var j=0; j<9; ++j) {
-      var elem = document.createElement('td');
-      elem.setAttribute('id', 'board-' + i + '-' + j);
-      elem.setAttribute('onclick', 'pressed(' + i + ',' + j + ');');
-      tr_elem.appendChild(elem);
+  if (board.getContext) {
+    var ctx = board.getContext('2d');
+    ctx.strokeStyle = 'rgb(102, 102, 102)';
+    for (var i=0; i<18; ++i) {
+      ctx.lineWidth = i % 9 == 0 ? 1.5 : 1;
+      ctx.beginPath();
+      ctx.moveTo(0, i * 35 + 17.5);
+      ctx.lineTo(630, i * 35 + 17.5);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(i * 35 + 17.5, 0);
+      ctx.lineTo(i * 35 + 17.5, 630);
+      ctx.stroke();
     }
-    board.appendChild(tr_elem);
+    board.setAttribute('style', 'clip-path: inset(0px 315px 315px 0px); top: 0px; left: 0px;');
+    board.setAttribute('onmousedown', 'mouse_click(event);');
+    board.setAttribute('onmousemove', 'mouse_move(event);');
+    board.setAttribute('onmouseup', 'mouse_up(event);');
+    pos_x = pos_y = 0;
+  } else {
+    for (var i=0; i<9; ++i) {
+      var tr_elem = document.createElement('tr');
+      for (var j=0; j<9; ++j) {
+        var elem = document.createElement('td');
+        elem.setAttribute('id', 'board-' + i + '-' + j);
+        elem.setAttribute('onclick', 'pressed(' + i + ',' + j + ');');
+        tr_elem.appendChild(elem);
+      }
+      board.appendChild(tr_elem);
+    }
   }
   board_num = [];
   for (var i=0; i<81; ++i) {
@@ -22,12 +45,67 @@ function init() {
   info.innerHTML = '로드되었습니다.';
 }
 
+function mouse_click(evn) {
+  click_x = Math.floor(evn.offsetX / 35) % 9;
+  click_y = Math.floor(evn.offsetY / 35) % 9;
+  clicked = true;
+  console.log(evn);
+}
+
+function mouse_move(evn) {
+  if (clicked) {
+    click_x = click_y = undefined;
+    pos_x -= evn.movementX;
+    pos_y -= evn.movementY;
+    if (pos_x < 0) {
+      pos_x += 315;
+    }
+    if (pos_x >= 315) {
+      pos_x -= 315;
+    }
+    if (pos_y < 0) {
+      pos_y += 315;
+    }
+    if (pos_y >= 315) {
+      pos_y -= 315;
+    }
+    var style = 'top: ' + (-pos_y) + 'px; left: ' + (-pos_x) + 'px; ';
+    style += 'clip-path: inset(' + pos_y + 'px ' + (315 - pos_x) + 'px ';
+    style += (315 - pos_y) + 'px ' + pos_x + 'px);';
+    board.setAttribute('style', style);
+  }
+}
+
+function mouse_up(evn) {
+  clicked = false;
+  console.log(click_x, click_y);
+  if (click_x !== undefined && click_y !== undefined) {
+    pressed(click_y, click_x);
+  }
+}
+
+function put_action(y, x, is_opponent) {
+  if (pos_x !== undefined) {
+    var ctx = document.getElementById('board').getContext('2d');
+    ctx.fillStyle =  is_opponent ? '#f66' : '#090';
+    for (var dx=0; dx<=2; ++dx) {
+      for (var dy=0; dy<=2; ++dy) {
+        ctx.beginPath();
+        ctx.arc(x * 35 + dx * 315 + 17, y * 35 + dy * 315 + 17, 15, 0, 2 * Math.PI, false);
+        ctx.fill();
+      }
+    }
+  } else {
+    var caller = document.getElementById('board-' + y + '-' + x);
+    caller.classList.add(is_opponent ? 'board-red' : 'board-green');
+  }
+}
+
 function pressed(y, x) {
   if (can_place && board_num[y * 9 + x] === 0) {
     can_place = false;
     info.innerHTML = 'AI가 생각하는 중입니다...';
-    var caller = document.getElementById('board-' + y + '-' + x);
-    caller.classList.add('board-green');
+    put_action(y, x, false);
     board_num[y * 9 + x] = 2;
     get_next();
   }
@@ -43,12 +121,13 @@ function get_next() {
         var pos = +arr[0];
         var y = Math.floor(pos / 9), x = pos % 9;
         if (pos !== -1) {
-          document.getElementById('board-' + y + '-' + x).classList.add('board-red');
+          put_action(y, x, true);
           board_num[pos] = 1;
           can_place = true;
         }
         var game_status = +arr[1];
         if (game_status >= 0) {
+          can_place = false;
           if (game_status === 0) {
             info.innerHTML = '비겼습니다.';
           } else if (game_status === 1) {
